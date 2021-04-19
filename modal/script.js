949894c58707368b7cb0825c83dc9885647dde5a
yoonpee <73012145+yoonpee@users.noreply.github.com>
2021-04-19 15:35:57 +0900

    Add files via upload

--- modal/script.js
+++ modal/script.js
@@ -1,24 +1,52 @@
 const modal = document.querySelector('.modal');
-const preview = document.querySelectorAll('.gallery img');
-const original = document.querySelector('.pic');
-const imagText = document.querySelector('.caption');
+const img = document.querySelectorAll('.gallery img');
+const pic = document.querySelector('.pic');
+const cap = document.querySelector('.caption');
 
-preview.forEach((prev)=>{
-  prev.addEventListener('click',()=>{
-      modal.classList.add('open');
-      original.classList.add('open');
+img.forEach((image)=>{
+    image.addEventListener('click', ()=>{
+        modal.classList.add('open');
+        pic.classList.add('open');
 
-      const originalSrc = prev.getAttribute("data-original");
-      original.src = `./image/${originalSrc}`;
-      
-      const altText = prev.alt;
-      imagText.innerText = altText;
-  })  
+        const picAlt = image.alt;
+        cap.innerText = picAlt;
+
+        const originSrc = image.getAttribute('data-original');
+        pic.src = `./image/${originSrc}`
+
+    })
 })
 
 modal.addEventListener('click', (e)=>{
     if(e.target.classList.contains('modal')){
         modal.classList.remove('open');
-        original.classList.remove('open');
     }
+    
 })
+
+
+
+// const modal = document.querySelector('.modal');
+// const preview = document.querySelectorAll('.gallery img');
+// const original = document.querySelector('.pic');
+// const imagText = document.querySelector('.caption');
+
+// preview.forEach((prev)=>{
+//   prev.addEventListener('click',()=>{
+//       modal.classList.add('open');
+//       original.classList.add('open');
+
+//       const originalSrc = prev.getAttribute("data-original");
+//       original.src = `./image/${originalSrc}`;
+      
+//       const altText = prev.alt;
+//       imagText.innerText = altText;
+//   })  
+// })
+
+// modal.addEventListener('click', (e)=>{
+//     if(e.target.classList.contains('modal')){
+//         modal.classList.remove('open');
+//         original.classList.remove('open');
+//     }
+// })
